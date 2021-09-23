a962a50046e1f10da3cbf2180d69369977c9df2e
marcelweidum <marcelweidum@live.nl>
2021-09-23 10:39:31 +0200

    Fix :)

--- config/twill.php
+++ config/twill.php
@@ -1,4 +1,16 @@
 <?php
 
 return [
+    'block_editor' => [
+        'crops' => [
+            'cat_image' => [
+                'default' => [
+                    [
+                        'name' => 'default',
+                        'ratio' => 1,
+                    ],
+                ]
+            ],
+        ]
+    ]
 ];
